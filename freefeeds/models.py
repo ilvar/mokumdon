f6537ccec738a10c127798644bdae1e7b5f277b1
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2020-06-28 15:15:19 +0100

    fix url

--- freefeeds/models.py
+++ freefeeds/models.py
@@ -46,8 +46,8 @@ class User(models.Model, FfToMdConvertorMixin):
             "following_count": 0,
             "statuses_count": 0,
             "note": "",
-            "uri": "https://freefeed.net/%s" % self.username,
-            "url": "https://freefeed.net/%s" % self.username,
+            "uri": "https://mokum.place/%s" % self.username,
+            "url": "https://mokum.place/%s" % self.username,
             "avatar": self.avatar_url,
             "avatar_static": self.avatar_url,
             "header": "",
@@ -119,9 +119,9 @@ class Post(models.Model, FfToMdConvertorMixin):
 
     def get_absolute_url(self):
         if self.parent is not None:
-            return "https://freefeed.net/%s/%s" % (self.parent.user.username, self.parent.feed_id)
+            return "https://mokum.place/%s/%s" % (self.parent.user.username, self.parent.feed_id)
         else:
-            return "https://freefeed.net/%s/%s" % (self.user.username, self.feed_id)
+            return "https://mokum.place/%s/%s" % (self.user.username, self.feed_id)
 
     def to_md_json(self):
         return {
