63ac34812112fde1c3e7aeb43d107756e2c14823
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2021-08-23 17:36:48 +0100

    Get back Rollbar

--- mokum/settings.py
+++ mokum/settings.py
@@ -117,3 +117,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if not DEBUG:
+    MIDDLEWARE = MIDDLEWARE + ['rollbar.contrib.django.middleware.RollbarNotifierMiddleware']
+    
+    ROLLBAR = {
+        'access_token': os.environ.get("ROLLBAR_TOKEN", "DUMMY"),
+        'environment': 'development' if DEBUG else 'production',
+        'root': BASE_DIR,
+    }
+    import rollbar
+    
+    rollbar.init(**ROLLBAR)
